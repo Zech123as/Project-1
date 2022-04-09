d06f1c17851448435ab69816c56116b493410f16
Zech123as <97283406+Zech123as@users.noreply.github.com>
2022-04-09 20:55:59 +0530

    Update Chart_Code.py

--- Chart_Code.py
+++ Chart_Code.py
@@ -55,7 +55,7 @@ if end_time_input == datetime(2021, 11, 4):
 IndexCSV  =  pd.DataFrame(td_obj.get_historic_data(Index_Name, duration='7 D', bar_size='EOD',   end_time = end_time_input))
 IndexCSV
 
-Expiry    =  IndexCSV[len(IndexCSV)-1]
+Expiry    =  Expiry_Date - timedelta(days = 1)
 
 st.write(Expiry, Expiry.strftime("%A"))
 
